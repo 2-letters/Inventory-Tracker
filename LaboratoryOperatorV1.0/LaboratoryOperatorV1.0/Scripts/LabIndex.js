90cf36b641c070f01da7ca6c37a53b65dabd47a0
RJ Varona <varonarudson@gmail.com>
2019-05-07 16:50:08 -0400

    made more custom cards

---
+++ LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Scripts/LabIndex.js
@@ -0,0 +1,21 @@
+﻿var x = model;
+
+
+new Vue({
+    el: '#app',
+    data: {
+        model: model,
+        name: model.Name,
+        description: model.description,
+        two: 2,
+        Assignments: [
+            { LabName: 'Lab 1: introduction', description: 'This is an introductory course' },
+            { LabName: 'Lab 2: Krichoffs law', description: 'Here we will test Kirchoff' },
+            { LabName: 'Lab 3: Intro to radiation', description: 'This is an introductory course' },
+            { LabName: 'Lab 4: Spectometer Lab', description: 'This is an introductory course' },
+            { LabName: 'Lab 5: Refraction of the indexes', description: 'This is an introductory course' },
+        ]
+
+
+    }
+})
