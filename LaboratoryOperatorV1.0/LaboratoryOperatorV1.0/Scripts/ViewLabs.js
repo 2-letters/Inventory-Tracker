904f3e43663a29e6334f14a4103d02b2271d69e1
RJ Varona <varonarudson@gmail.com>
2019-05-10 13:04:02 -0400

    tried some stuff

--- LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Scripts/ViewLabs.js
+++ LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Scripts/ViewLabs.js
@@ -7,7 +7,7 @@
                 { text: 'Equipment', value: 'equipment' },
                 { text: 'Location', value: 'location' },
                 { text: 'Qty', value: 'qty' },
-                {text: 'Add or Remove', value: 'add_or_remove'}
+                {text: 'Add or Remove', value: 'addo'}
             ],
             items: [
                 { equipment: 'vernier-caliber', location: 'thatcher south room 3535  drawer 20', qty: 88 },
@@ -29,3 +29,56 @@
         }
     }
 })
+
+
+//new Vue({
+//    el: '#app',
+//    //vuetify: new Vuetify(),
+
+//    data: () => ({
+
+//        search: '',
+//        Headers: [
+//            { text: 'Equipment', value: 'equipment' },
+//            { text: 'Location', value: 'location' },
+//            { text: 'Qty', value: 'qty' },
+//            { text: 'Add or Remove', value: 'add' }
+//        ],
+//        equipments: [],
+//        editedIndex: -1,
+//        editedItem: {
+//            name: '',
+//            location: '',
+//            Qty: 0
+//        }
+//    }),
+
+
+//    created() {
+//        this.initialize();
+//    },
+
+//    methods: {
+//        initialize() {
+//            this.equipment = [
+//                { equipment: 'vernier-caliber', location: 'thatcher south room 3535  drawer 20', qty: 88 },
+
+//                { equipment: 'ruler', location: 'hickman  room 4535  drawer 20', qty: 2 },
+
+//                { equipment: 'firebase', location: 'thatcher south room 3535  drawer 20', qty: 420 },
+
+//                { equipment: 'is beter than', location: 'thatcher south room 3535  drawer 20', qty: 62 },
+
+//                { equipment: 'aws cuz its free', location: 'thatcher south room 3535  drawer 20', qty: 68 }
+//            ]
+//        }
+//    },
+    
+//    decrement(item) {
+//        this.editedIndex = this.equipments.indexOf(item)
+//        this.editedItem = Object.assign({}, item)
+//        this.dialog = true
+
+//    }
+//})
+
