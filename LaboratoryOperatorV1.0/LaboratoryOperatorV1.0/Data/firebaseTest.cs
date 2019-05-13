54831361c2920e8d49306c1b2f6ecdacb645dfe5
rjvarona <rjvarona16@gmail.com>
2019-05-13 00:12:22 -0400

    trying to implement firebase

---
+++ LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Data/firebaseTest.cs
@@ -0,0 +1,22 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Database.Query;
+using System.Web;
+
+namespace LaboratoryOperatorV1._0.Data
+{
+    public class firebaseTest
+    {
+        var auth = "ABCDE"; // your app secret
+        var firebaseClient = new FirebaseClient(
+          "<URL>",
+          new FirebaseOptions
+          {
+              AuthTokenAsyncFactory = () => Task.FromResult(auth)
+          });
+
+    }
+}
