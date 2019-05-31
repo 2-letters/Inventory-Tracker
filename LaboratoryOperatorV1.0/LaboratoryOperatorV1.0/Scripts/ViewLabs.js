a1bc455b8d1e7683d20a6b1c63bf5787e34658fd
rjvarona <rjvarona16@gmail.com>
2019-05-31 00:03:10 -0400

    hardest javascript implementation i have ever done

--- LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Scripts/ViewLabs.js
+++ LaboratoryOperatorV1.0/LaboratoryOperatorV1.0/Scripts/ViewLabs.js
@@ -1,7 +1,4 @@
-﻿
-
-
-new Vue({
+﻿new Vue({
     el: '#app',
     model: model,
     data() {
@@ -21,7 +18,7 @@ new Vue({
                 { text: 'Equipment', value: 'equipment' },
                 { text: 'Location', value: 'location' },
                 { text: 'Available', value: 'qty' },
-                { text: 'MoreInfo', value: 'pictureUrl',  sortable: false, disabled: true },
+                { text: 'MoreInfo', value: 'pictureUrl', sortable: false, disabled: true },
                 { text: 'Add or Remove', value: 'addo', sortable: false, disabled: true }
             ],
             HeadersAdded: [
@@ -38,7 +35,7 @@ new Vue({
 
     mounted() {
         for (var i = 0; i < model.IndexList.length; i++) {
-            this.items.push({ equipment: model.IndexList[i].itemName, location: model.IndexList[i].location, qty: model.IndexList[i].quantity, id: model.IndexList[i].id, i: i, pictureUrl: model.IndexList[i].pictureUrl, description: model.IndexList[i].description })
+            this.items.push({ equipment: model.IndexList[i].itemName, location: model.IndexList[i].location, qty: model.IndexList[i].quantity, id: model.IndexList[i].id, i: i, pictureUrl: model.IndexList[i].pictureUrl, description: model.IndexList[i].description, foo: 0, prevFoo: 0, original: model.IndexList[i].quantity })
             //this.itemsAdded.push({ equipmentAdded: model.IndexList[i].itemName, locationAdded: model.IndexList[i].location, qtyAdded: model.IndexList[i].quantity, id: model.IndexList[i].id })
 
         }
@@ -53,18 +50,76 @@ new Vue({
             var isAdded = this.itemsAdded.some(e => e.id === id);
 
             var result = this.items.filter(obj => { return obj.id === id; });
-            
-            if (isAdded === false) {
 
-                this.itemsAdded.push({ itemName: result[0].equipment, location: result[0].location, quantity: 1, id: result[0].id });
-                this.items[result[0].i].qty--;
-            }
-            else {
+
+            if (isAdded) {
+
                 var index = this.itemsAdded.map(function (e) { return e.id; }).indexOf(id);
-                this.itemsAdded[index].quantity++;
-                this.items[result[0].i].qty--;
+                if (this.items[result[0].i].foo !== this.items[result[0].i].prevFoo) {
+                    if ((this.items[result[0].i].foo > this.items[result[0].i].original)) {
+                        this.items[result[0].i].foo = this.items[result[0].i].prevFoo;
+                        return;
+                    }
+                    else {
+                        this.itemsAdded[index].quantity = this.items[result[0].i].foo;
+                        this.items[result[0].i].qty = this.items[result[0].i].original - this.itemsAdded[index].quantity;
+                    }
+                }
+                else
+                {
+                    this.itemsAdded[index].quantity++;
+                    this.items[result[0].i].qty--;
+                }
+
+              
             }
-            
+            else
+            {
+                var x = this.items[result[0].i].foo;
+                if (this.items[result[0].i].foo !== this.items[result[0].i].prevFoo)
+                {
+                    if ((this.items[result[0].i].foo > this.items[result[0].i].original) || (this.items[result[0].i].foo < -1)) {
+                        this.items[result[0].i].foo = this.items[result[0].i].prevFoo;
+                        return;
+                    }
+                    else
+                    {
+                        
+                        this.itemsAdded.push({ itemName: result[0].equipment, location: result[0].location, quantity: this.items[result[0].i].foo, id: result[0].id });
+                        this.items[result[0].i].qty = this.items[result[0].i].qty - this.items[result[0].i].foo;
+                    }
+                }
+                else {
+
+
+                    this.itemsAdded.push({ itemName: result[0].equipment, location: result[0].location, quantity: 1, id: result[0].id });
+                    this.items[result[0].i].qty--;
+                }
+            }
+
+
+
+
+
+            //if (this.items[result[0].i].foo !== this.items[result[0].i].prevFoo) {
+
+            //    if ((this.items[result[0].i].foo + this.itemsAdded[index].quantity) > this.items[result[0].i].original) {
+            //        this.items[result[0].i].foo = this.items[result[0].i].prevFoo;
+            //        return;
+            //    }
+            //}
+
+            //if (isAdded === false) {
+
+            //    this.itemsAdded.push({ itemName: result[0].equipment, location: result[0].location, quantity: 1, id: result[0].id });
+            //    this.items[result[0].i].qty--;
+            //}
+            //else {
+            //    var index = this.itemsAdded.map(function (e) { return e.id; }).indexOf(id);
+            //    this.itemsAdded[index].quantity++;
+            //    this.items[result[0].i].qty--;
+            //}
+
 
 
             if (this.items[result[0].i].qty <= 0) {
@@ -72,9 +127,15 @@ new Vue({
             }
             else {
                 this.disabled = "";
-                
-               
+
+
             }
+            this.items[result[0].i].foo = this.itemsAdded[index].quantity;
+
+
+
+            this.items[result[0].i].prevFoo = this.items[result[0].i].foo;
+
 
         },
         decrement: function (id) {
@@ -97,39 +158,38 @@ new Vue({
 
 
             if (this.itemsAdded[index].quantity <= 0) {
+                this.items[result[0].i].foo = 0;
                 this.disabled = id;
                 this.itemsAdded.splice(index, 1);
             }
             else {
                 this.disabled = "";
 
+                this.items[result[0].i].foo = this.itemsAdded[index].quantity;
+            }
 
-            }
-           
         },
-       
-        submitData: function (){
-        $.ajax({
-            type: 'POST',
-            url: '/Home/PushNewLab',
-            data: {
-                labName: $("#labName").val(),
-                labDescription: $("#labDescription").val(),
-                itemsInLab: this.itemsAdded
-            },
-            success: function (data) {
-                $('#results').html(data)
-            },
-            error: function (data) {
-                console.log('Error, please report to a developer')
-            }
-        });
-    }
+
+        submitData: function () {
+            $.ajax({
+                type: 'POST',
+                url: '/Home/PushNewLab',
+                data: {
+                    labName: $("#labName").val(),
+                    labDescription: $("#labDescription").val(),
+                    itemsInLab: this.itemsAdded
+                },
+                success: function (data) {
+                    $('#results').html(data)
+                },
+                error: function (data) {
+                    console.log('Error, please report to a developer')
+                }
+            });
+        }
 
 
 
     }
 })
-
-
 
